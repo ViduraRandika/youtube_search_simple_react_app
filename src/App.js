0f7b37fac984888177ef3d23684692be08fb32f3
ViduraRandika <cst17047@std.uwu.ac.lk>
2022-01-13 20:45:55 +0530

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'React';
+import {Grid} from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    render(){
+        return(
+            <Grid>
+                <h1>App component</h1>
+                <p>Project initialization</p>
+            </Grid>
+        )
+    }
 }
 
 export default App;
