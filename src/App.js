e29705f9659a7f74aeb6e8af5e645107fa7f8d03
ViduraRandika <cst17047@std.uwu.ac.lk>
2022-01-13 21:08:30 +0530

    main grid structure added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'React';
+import React from 'react';
 import {Grid} from '@material-ui/core';
 
 class App extends React.Component{
     render(){
         return(
-            <Grid>
-                <h1>App component</h1>
-                <p>Project initialization</p>
+            <Grid justifyContent='center' container spacing={10}>
+                <Grid item xs={12}>
+                    <Grid container spacing={10} >
+                        <Grid item xs={12}><h1>SEARCH BAR</h1></Grid>
+                        <Grid item xs={8}><h1>MAIN VIDEO PLAYER</h1></Grid>
+                        <Grid item xs={4}><h1>Video list</h1></Grid>
+                    </Grid>
+                </Grid>
             </Grid>
         )
     }
